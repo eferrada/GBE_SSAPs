21f643cad16a660f5ff37aecb83b4f08b56e52e6
eferrada <eferrada@users.noreply.github.com>
2019-01-23 15:10:23 -0300

    Update KLDiv.m

--- CODE2/KLDiv.m
+++ CODE2/KLDiv.m
@@ -1,13 +1,10 @@
-function dist=KLDiv(P,Q)
-%  dist = KLDiv(P,Q) Kullback-Leibler divergence of two discrete probability
-%  distributions
-%  P and Q  are automatically normalised to have the sum of one on rows
-% have the length of one at each 
+function [ d ] = KLDiv (P,Q)
+%  d = KLDiv(P,Q) Kullback-Leibler divergence of two discrete probability
+%  distributions P and Q. Distributions are normalised to sum of one and to
+%  have the length of one at each 
 % P =  n x nbins
 % Q =  1 x nbins or n x nbins(one to one)
-% dist = n x 1
-
-
+% d = n x 1
 
 if size(P,2)~=size(Q,2)
     error('the number of columns in P and Q should be the same');
@@ -21,15 +18,16 @@ end
 if size(Q,1)==1
     Q = Q ./sum(Q);
     P = P ./repmat(sum(P,2),[1 size(P,2)]);
-    dist =  sum(P.*log(P./repmat(Q,[size(P,1) 1])),2);
+    d =  sum(P.*log(P./repmat(Q,[size(P,1) 1])),2);
     
 elseif size(Q,1)==size(P,1)
     
     Q = Q ./repmat(sum(Q,2),[1 size(Q,2)]);
     P = P ./repmat(sum(P,2),[1 size(P,2)]);
-    dist =  sum(P.*log(P./Q),2);
+    d =  sum(P.*log(P./Q),2);
 end
 
 % resolving the case when P(i)==0
-dist(isnan(dist))=0;
+d(isnan(d))=0;
 
+end
